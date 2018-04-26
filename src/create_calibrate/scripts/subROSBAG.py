d79cd972787bd2f71a9b065f08b88868128a9373
rdensamo <rbd218@lehigh.edu>
2018-04-26 17:29:41 -0400

    all librarys functional

--- src/create_calibrate/scripts/subROSBAG.py
+++ src/create_calibrate/scripts/subROSBAG.py
@@ -1,20 +1,29 @@
 #!/usr/bin/env python3
+# PYTHON3 CAUSING ERRORS WITH ROSPY AND ROSLIB
 # Above line indicates script is executed as a Python script using Python3
 # This should be run using Python 2
 # This ros publisher will publish the rosbag pointcloud data
 
 # Must import rospy when writing a ROS Node
+# Note: Python 3 does not suppoter ROSBAG DO NOT IMPORT
 import rospy
-import rosbag
 import roslib
+import pcl
 # Allows us to reuse message types for publishing
 from std_msgs.msg import String
 from sensor_msgs.msg import PointCloud2
 from sensor_msgs.msg import Image
 
+
+# callback function gets called when a new message arrives on topic
 def callback(data):
 	#rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)  # TODO: Revisit This
-        rospy.loginfo(rospy.get_caller_id() + "I heard ") 
+        rospy.loginfo("received data!")
+        # A subscriber can get access to "conection header"
+        # This will let us see on which topic the message is from
+        # to ensure we are getting messages from both topics we are publishing on
+        # for debugging only, gives gross output
+        # print(data._connection_header)
 
 def subROSBAG():
 
